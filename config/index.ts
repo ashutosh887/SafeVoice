18452ccf320747df39ba45e691904b6a5f6c745b
Ashutosh Jha <ashutoshj887@gmail.com>
2025-12-30 12:52:27 +0530

    fix: update app tagline

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 export default {
     appName: "SafeVoice",
-    appTagLine: "Your invisible legal companion",
+    appTagLine: "A private space for reflection",
     appDescription: "AI-Powered Legal Evidence Documentation for Victims of Abuse",
     appAuthor: "ashutosh887",
     appAuthorUrl: "https://github.com/ashutosh887",
