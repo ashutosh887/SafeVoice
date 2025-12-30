e34ebbc298030f6cd9310b3ff3bb47cd7ec1de6b
Ashutosh Jha <ashutoshj887@gmail.com>
2025-12-30 14:40:12 +0530

    feat: add component for handling safe area insets

---
+++ components/SafeScreen.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function SafeScreen({ children }: PropsWithChildren) {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        backgroundColor: "#ffffff",
+      }}
+    >
+      {children}
+    </View>
+  );
+}
