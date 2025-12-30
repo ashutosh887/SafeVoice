e0cc377985db7b762cdd3f5b71b52db9fc1a3d56
Ashutosh Jha <ashutoshj887@gmail.com>
2025-12-30 16:11:12 +0530

    refactor: update navigation behavior

--- app/meditation.tsx
+++ app/meditation.tsx
@@ -1,8 +1,7 @@
 import { useRouter } from "expo-router";
-import React from "react";
 import { Pressable, Text, View } from "react-native";
 
-const Meditation = () => {
+export default function Meditation() {
   const router = useRouter();
 
   return (
@@ -19,15 +18,13 @@ const Meditation = () => {
       <View className="w-32 h-32 rounded-full bg-gray-200 mb-12" />
 
       <Pressable
-        onPress={() => router.back()}
-        className="px-6 py-3 rounded-xl bg-black"
+        onPress={() => router.replace("/")}
+        className="px-6 py-3 rounded-xl bg-black w-[200px]"
       >
-        <Text className="text-white font-medium">
+        <Text className="text-white font-medium text-center">
           Done
         </Text>
       </Pressable>
     </View>
   );
-};
-
-export default Meditation;
+}
